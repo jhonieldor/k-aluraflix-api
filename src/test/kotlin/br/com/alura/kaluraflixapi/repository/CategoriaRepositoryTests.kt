bd1e304da26e6b5043bf66ea7937de6fae2f5d7d
jhonieldor <jhonieldor@gmail.com>
2021-08-17 23:18:32 -0300

    Reativação parcial da classe de teste de CategoriaRepository

--- src/test/kotlin/br/com/alura/kaluraflixapi/repository/CategoriaRepositoryTests.kt
+++ src/test/kotlin/br/com/alura/kaluraflixapi/repository/CategoriaRepositoryTests.kt
@@ -1,43 +1,43 @@
-//package br.com.alura.kaluraflixapi.repository
-//
-//import br.com.alura.kaluraflixapi.model.Categoria
-//import org.junit.jupiter.api.AfterAll
-//import org.junit.jupiter.api.Assertions.*
-//import org.junit.jupiter.api.BeforeAll
-//import org.junit.jupiter.api.Test
-//import org.junit.jupiter.api.TestInstance
-//import org.springframework.beans.factory.annotation.Autowired
-//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
-//import org.springframework.boot.test.context.SpringBootTest
-//import org.springframework.data.domain.Pageable
-//import org.springframework.data.repository.findByIdOrNull
-//import org.springframework.test.context.ActiveProfiles
-//
-//@SpringBootTest
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-//@ActiveProfiles("test")
-//class CategoriaRepositoryTests {
-//
-//    val paginacao = Pageable.ofSize(100)
-//
-//    @Autowired
-//    val categoriaRepository: CategoriaRepository? = null
-//
-//    @Autowired
-//    val agrupamentoVideosCategoriaRepository: AgrupamentoVideosCategoriaRepository? = null
-//
-//    @Autowired
-//    val videoRepository: VideoRepository? = null
-//
-//    var categoria: Categoria? = Categoria(id = 0, titulo = "CATEGORIA TESTE", cor = "#FFF")
-//
-//    @BeforeAll
-//    fun createCategoriaMustPass(){
-//        categoria = categoriaRepository?.save(categoria!!)
-//        assertNotNull(categoria)
-//    }
-//
+package br.com.alura.kaluraflixapi.repository
+
+import br.com.alura.kaluraflixapi.model.Categoria
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("test")
+class CategoriaRepositoryTests {
+
+    val paginacao = Pageable.ofSize(100)
+
+    @Autowired
+    val categoriaRepository: CategoriaRepository? = null
+
+    @Autowired
+    val agrupamentoVideosCategoriaRepository: AgrupamentoVideosCategoriaRepository? = null
+
+    @Autowired
+    val videoRepository: VideoRepository? = null
+
+    var categoria: Categoria? = Categoria(id = 0, titulo = "CATEGORIA TESTE", cor = "#FFF")
+
+    @BeforeAll
+    fun createCategoriaMustPass(){
+        categoria = categoriaRepository?.save(categoria!!)
+        assertNotNull(categoria)
+    }
+
 //    @AfterAll
 //    fun removeCategoriaMustPass(){
 //        val id = categoria!!.id
@@ -75,4 +75,4 @@
 //            println("videos: " + content.videos.size)
 //        }
 //    }
-//}
+}
